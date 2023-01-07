3777d23f309e4786b1fcecae15a9eec2a13a28ca
pavlinamarkova <119419872+pavlinamarkova@users.noreply.github.com>
2023-01-07 22:16:06 +0100

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -2,25 +2,10 @@ $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-let cookieModal = document.querySelector(".cookie-consent-modal")
-let cancelCookieBtn = document.querySelector(".btn.cancel")
-let acceptCookieBtn = document.querySelector(".btn.accept")
-
-cancelCookieBtn.addEventListener("click", function (){
-    cookieModal.classList.remove("active")
-})
-acceptCookieBtn.addEventListener("click", function (){
-    cookieModal.classList.remove("active")
-    localStorage.setItem("cookieAccepted", "yes")
-})
-
-setTimeout(function (){
-    let cookieAccepted = localStorage.getItem("cookieAccepted")
-    if (cookieAccepted != "yes"){
-        cookieModal.classList.add("active")
-    }
-}, 2000)
+$(window).on('load', function () {
+    $('#exampleModal').modal('show');
+});
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
